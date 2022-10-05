c87d9f4d6837315e8015b88b3ba065418a0a276d
Marco Beduschi <8469440+marco-beduschi@users.noreply.github.com>
2022-10-05 16:57:56 -0300

    chore: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,48 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Task.create!(
+  description: "Read Ailton Krenak's book 'Ideias para adiar o fim do mundo'"
+)
+
+Task.create!(
+  description: "Read Bell Hooks book 'Tudo Sobre Amor'"
+)
+
+Task.create!(
+  description: "Re-read John Green's book 'Anthropocene Reviewed'"
+)
+
+Task.create!(
+  description: "Read Michael Chabon's book 'The Mysteries of Pittsburgh'"
+)
+
+Task.create!(
+  description: "Watch 'Aronofski's The Whale' movie"
+)
+
+Task.create!(
+  description: 'Take bicycle to workshop for yearly review'
+)
+
+Task.create!(
+  completed: true,
+  description: 'Take dog for a walk'
+)
+
+Task.create!(
+  completed: true,
+  description: "Read 'Dune'"
+)
+
+Task.create!(
+  completed: true,
+  description: "Vote on Brazil's Election 2022"
+)
+
+Task.create!(
+  completed: true,
+  description: "Pay credit card's invoice"
+)
